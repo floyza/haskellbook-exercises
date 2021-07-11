2099bfdafa14d28a26a47316ab536ddf7cb14cae
Gavin Downard <gavin.downard@runbox.com>
2021-07-10 17:54:41 -0700

    Finish chapter 12

--- SignalingAdversity-12/EitherLib.hs
+++ SignalingAdversity-12/EitherLib.hs
@@ -1,16 +1,29 @@
 module SignalingAdversity.EitherLib where
 
+import Control.Arrow
+
 lefts' :: [Either a b] -> [a]
---lefts' ((Left x):xs) = x:(lefts' xs)
---lefts' (_:xs) = lefts' xs
---lefts' [] = []
+lefts' = foldr f []
+  where
+    f (Left x) = (x :)
+    f (Right _) = id
 
---lefts' = foldr (\ x acc ->
---                  case x of
---                  Left x -> x:acc
---                  Right _ -> acc)
---               []
+rights' :: [Either a b] -> [b]
+rights' = foldr f []
+  where
+    f (Right x) acc = x : acc
+    f (Left _) acc = acc
 
-lefts' = foldr (flip (\acc -> (either (:acc) (const acc)))) []
+partitionEithers' :: [Either a b] -> ([a], [b])
+partitionEithers' = lefts' &&& rights'
 
---rights' :: [Either a b] -> [b]
+eitherMaybe' :: (b -> c) -> Either a b -> Maybe c
+eitherMaybe' f (Right x) = Just (f x)
+eitherMaybe' _ (Left _) = Nothing
+
+either' :: (a -> c) -> (b -> c) -> Either a b -> c
+either' f _ (Left x) = f x
+either' _ f (Right x) = f x
+
+eitherMaybe'' :: (b -> c) -> Either a b -> Maybe c
+eitherMaybe'' f = either' (const Nothing) (Just . f)
--- SignalingAdversity-12/StringProcessing.hs
+++ SignalingAdversity-12/StringProcessing.hs
@@ -1,7 +1,30 @@
 module SignalingAdversity.StringProcessing where
 
-notThe :: String -> String
-notThe :: (=="the")
+import Data.Maybe (fromMaybe)
+
+notThe :: String -> Maybe String
+notThe "the" = Nothing
+notThe s = Just s
 
 replaceThe :: String -> String
-replaceThe
+replaceThe = unwords . map (fromMaybe "a" . notThe) . words
+
+isVowel :: Char -> Bool
+isVowel 'a' = True
+isVowel 'e' = True
+isVowel 'i' = True
+isVowel 'o' = True
+isVowel 'u' = True
+isVowel _ = False
+
+countTheBeforeVowel :: String -> Integer
+countTheBeforeVowel s = go (words s)
+  where
+    startsVowel = isVowel . head
+    -- no word starting with a vowel is "the", so we can call 'go xs' instead of 'go (s:xs)'
+    go ("the" : s : xs) = (if startsVowel s then 1 else 0) + go xs
+    go (_ : xs) = go xs
+    go [] = 0
+
+countVowels :: String -> Integer
+countVowels = toInteger . length . filter isVowel
